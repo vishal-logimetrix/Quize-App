5dc2788fab72319df0d94e43597c679c6ddeb415
vishal <vishallengare61@gmail.com>
2024-01-15 14:20:45 +0530

    add the AOS scrolling animation for about-us component

--- src/app/home/home/about-us/about-us.component.ts
+++ src/app/home/home/about-us/about-us.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css']
 })
-export class AboutUsComponent {
+export class AboutUsComponent implements OnInit {
+  ngOnInit(): void {
+  
+  }
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      AOS.init();
+    }, 300); 
+  }
 
 }
